9d95855e87bea80c0e3ea7ccc2c3ab0ba1d54248
Anna Ryazantceva <anyariazantzeva@yandex.ru>
2020-02-13 12:25:41 +0200

    Created basic functionality for todo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import {Todo} from './todo';
+import { TodoDataService } from './todo-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [TodoDataService]
 })
 export class AppComponent {
-  title = 'todo-list';
+  newTodo: Todo = new Todo();
+
+  constructor(private todoDataService: TodoDataService) {
+  }
+
+  toggleTodoComplete(todo) {
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  addTodo() {
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  removeTodo(todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoDataService.getAllTodos();
+  }
+  getCompletedTodos (todos) {
+    console.log(this.todoDataService.getCompletedTodos(todos));
+    return this.todoDataService.getCompletedTodos(todos);
+  }
+  getNotCompletedTodos (todos) {
+    console.log(this.todoDataService.getNotCompletedTodos(todos));
+    return this.todoDataService.getNotCompletedTodos(todos);
+  }
+
 }
