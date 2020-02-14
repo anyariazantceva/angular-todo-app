9d924a42b6778dcdc34ee5bf12b672c4df0a1064
Anna Ryazantceva <anyariazantzeva@yandex.ru>
2020-02-14 21:28:28 +0200

    Code refactoring

---
+++ precache-config.js
@@ -0,0 +1,22 @@
+let SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+
+module.exports = {
+  navigateFallback: '/index.html',
+  navigateFallbackWhitelist: [/^(?!\/__)/],
+  stripPrefix: 'dist',
+  root: 'dist/',
+  plugins: [
+    new SWPrecacheWebpackPlugin ({
+      cacheId: 'todo-app',
+      filename: 'service-worker.js',
+      staticFileGlobs: [
+        'dist/index.html',
+        'dist/**.js',
+        'dist/**.css'
+      ],
+      stripPrefix: 'dist/',
+      mergeStaticsConfig: true
+    }),
+  ]
+};
+
