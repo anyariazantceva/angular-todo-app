215329e6b1e529274fc8fc42a5d9a6ef0c224795
Anna Ryazantceva <anyariazantzeva@yandex.ru>
2020-02-16 16:29:24 +0200

    Added firebase and created components for login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,15 +11,38 @@ import { TodoListHeaderComponent } from './todo-list-header/todo-list-header.com
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoListItemComponent } from './todo-list-item/todo-list-item.component';
 import { MatInputModule } from '@angular/material/input';
+import { AngularFireModule } from '@angular/fire/';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { LoginComponent } from './admin/login/login.component';
+import { RegisterComponent } from './admin/register/register.component';
+import { ForgotPasswordComponent } from './admin/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './admin/verify-email/verify-email.component';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBmhtu2HUYh4lSRGjnwJCfVOvTluZenets',
+  authDomain: 'todo-app-86202.firebaseapp.com',
+  databaseURL: 'https://todo-app-86202.firebaseio.com',
+  projectId: 'todo-app-86202',
+  storageBucket: 'todo-app-86202.appspot.com',
+  messagingSenderId: '776426215499',
+  appId: '1:776426215499:web:79843d1056200b0ca015ce',
+  measurementId: 'G-NKJWCYPKBV'
+};
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoListHeaderComponent,
     TodoListComponent,
-    TodoListItemComponent
+    TodoListItemComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgotPasswordComponent,
+    VerifyEmailComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
     BrowserModule,
     AppRoutingModule,
     FormsModule,
